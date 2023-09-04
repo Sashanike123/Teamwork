12b930995e7395c8a7a1f2e8916579c728a7ba5e
Oleksandra <112472185+Sashanike123@users.noreply.github.com>
2023-09-04 22:13:23 +0200

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -8,18 +8,18 @@ class FinalWin(QWidget):
         super().__init__()
         self.initUI()
         self.set_appear()
-        self.shoe()
+        self.show()
     
     def initUI(self):
         self.workh_text = QLabel(txt_workheart)
         self.index_text = QLabel(txt_index)
 
-        self.layout_line = QVBoxLayout()
+        self.Layout_line = QVBoxLayout()
         self.Layout_line.addWidget(self.index_text, alignment=Qt.AlignCenter)
         self.Layout_line.addWidget(self.workh_text, alignment=Qt.AlignCenter)
-        self.setLayout(self.layout_line)
+        self.setLayout(self.Layout_line)
 
     def set_appear(self):
-        self.setWindowritle(txt_finalwin)
+        self.setWindowTitle(txt_finalwin)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)

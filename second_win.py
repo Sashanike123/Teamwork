7549e7f310308c02f0ee5232dfdd6568fde065a2
Oleksandra <112472185+Sashanike123@users.noreply.github.com>
2023-09-04 22:12:17 +0200

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,10 +1,11 @@
 # напиши здесь код для второго экрана приложения
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit, QHBoxLayout
 from instr import *
 from final_win import *
 class TestWin(QWidget):
     def __init__(self):
+        super().__init__()
         self.initUI()
         self.connects()
         self.set_appear()
@@ -14,8 +15,8 @@ class TestWin(QWidget):
         self.btn_test1 = QPushButton(txt_starttest1)
         self.btn_test2 = QPushButton(txt_starttest2)
         self.btn_test3 = QPushButton(txt_starttest3)
-        self.test_name = QLabel(txt_name)
-        self.test_age = QLabel(txt_age)
+        self.text_name = QLabel(txt_name)
+        self.text_age = QLabel(txt_age)
         self.text_test1 = QLabel(txt_test1)
         self.text_test2 = QLabel(txt_test2)
         self.text_test3 = QLabel(txt_test3)
@@ -27,7 +28,7 @@ class TestWin(QWidget):
         self.line_test3 = QLineEdit(txt_hinttest3)
         self.l_line = QVBoxLayout()
         self.r_line = QVBoxLayout()
-        self.h_line = QVBoxLayout()
+        self.h_line = QHBoxLayout()
         self.r_line.addWidget(self.text_timer, alignment = Qt.AlignCenter)
         self.l_line.addWidget(self.text_name, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.line_name, alignment = Qt.AlignLeft)
@@ -54,4 +55,4 @@ class TestWin(QWidget):
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
-        self.move(win_x, winx_y)
+        self.move(win_x, win_y)
